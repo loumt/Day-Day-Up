0b099f88c36b01aedd3f8ac4c63227bbcc8becf9
loumt <loumt@sanlogic.com>
2019-01-22 11:19:39 +0800

    1.icon

--- src/day-day-up-back-server/app.js
+++ src/day-day-up-back-server/app.js
@@ -18,7 +18,7 @@ app.use(compression())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger.access());
-// app.use(favicon(path.join(__dirname, 'public', './images/icon.png')));
+app.use(favicon(path.join(__dirname, 'public', './images/icon.ico')));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
